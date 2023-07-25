6a6b80ffb2111feb28a3bfe230c951b4dd85f8ec
4seokhyeon <tktjrgus@naver.com>
2023-07-25 10:54:26 +0900

    장바구니 부분 커밋

--- app/src/main/java/com/example/kiosk/KioskMain.kt
+++ app/src/main/java/com/example/kiosk/KioskMain.kt
@@ -15,9 +15,9 @@ fun main() {
 
     println("현재 잔액을 입력하세요")
     var cash = scanner.nextInt()
-    val won = "w"
+    val won = "원"
 
-    while (true){
+    while (true) {
         println("\" 공차에 오신걸 환영합니다 \"")
         println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요")
         println("[ 공차 메뉴 ]")
@@ -32,31 +32,61 @@ fun main() {
         print("선택:")
         val choice = scanner.nextInt()
 
-        when(choice){
+        when (choice) {
             1 -> {
-               bestcomMunu.displayMenu()
+                bestcomMunu.displayMenu()
                 print("선택 ")
                 val bestcomChoice = scanner.nextInt()
-                if(bestcomChoice == 0 ) continue
+                if (bestcomChoice == 0) continue
                 val menuItem = bestcomMunu.getMenuItem(bestcomChoice)
-                if (menuItem != null){
+                if (menuItem != null) {
                     order.addToOrder(menuItem)
-                }else{
+                } else {
                     println("잘못된 번호를 입력했어요. 다시 입력해주세요")
                 }
             }
-            2 ->{
+
+            2 -> {
                 originalTMenu.displayMenu()
                 print("선택 ")
                 val originalTChoice = scanner.nextInt()
-                if(originalTChoice == 1) continue
+                if (originalTChoice == 1) continue
                 val menuItem = originalTMenu.getMenuItem(originalTChoice)
-                if(menuItem !=null){
+                if (menuItem != null) {
                     order.addToOrder(menuItem)
-                }else{
+                } else {
                     println("잘못된 번호를 입력했어요. 다시 입력해주시요")
                 }
             }
+
+            6 -> {
+                if(order.isEmpty()){
+                    println("장바구니가 비어있습니다.")
+                    println("=====메뉴를 골라주세요")
+                    continue
+                }
+                order.displayOrder()
+                println("[ 장바구니 ]")
+                println("${order.getTotalPrice()}" + won)
+                println("1.  주문    2. 추가메뉴")
+                print("주문하려면 1번 다른 메뉴를 보고 싶으면 2번을 눌러주세요: ")
+                val orderChoice = scanner.nextInt()
+                when (orderChoice) {
+                    1 -> {
+                        if (order.getTotalPrice() <= cash) {
+                            cash -= order.getTotalPrice()
+                            println("주문이 완료되었습니다. 현재 잔액은 $cash 원 입니다.")
+                            order.clearOrder()
+                        } else {
+                            println("현재 잔액은 ${cash}원 으로 ${order.getTotalPrice() - cash}원이 부족하여 결제를 할 수 없습니다.")
+                        }
+                    }
+
+                    2 -> continue
+                    else -> println("잘못된 번호를 입력했어요. 다시 입력해주세요")
+                }
+            }
+
         }
     }
 
--- app/src/main/java/com/example/kiosk/Order.kt
+++ app/src/main/java/com/example/kiosk/Order.kt
@@ -8,19 +8,29 @@ class Order {
         println("${item.name}가 장바구니에 추가 되었습니다.")
     }
 
-    fun displayOrder(){
-        println("[ Orders ]")
-        for(item in orderList){
-            println("${item.name}   | ${item.price}  | ")
+    fun displayOrder() {
+        if (orderList.isEmpty()) {
+            println("장바구니가 비어있습니다.")
+            println("====메뉴를 선택해주세요====")
+        } else {
+            println("[ | 장바구니 | ]")
+            for (item in orderList) {
+                println("${item.name}   | ${item.price}  원  | ")
+            }
         }
     }
 
-    fun getRotalPrice(): Int{
+
+    fun getTotalPrice(): Int{
         return orderList.sumOf { it.price }
     }
 
     fun clearOrder(){
         orderList.clear()
-        println(" 주문이 취소되었습니다.")
+        println(" 주문이 완료되었습니다.")
+    }
+
+    fun isEmpty(): Boolean {
+        return orderList.isEmpty()
     }
 }
