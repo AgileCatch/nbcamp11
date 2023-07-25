b11d0a003d08f135887b36e73c4ca4076e3f05e5
yeonghyeon <agilecatch@gmail.com>
2023-07-25 10:47:21 +0900

    [FEAT] : ⚙️
    공차 메뉴 추가 완료

--- app/src/main/java/com/example/kiosk/MilkTMenu.kt
+++ app/src/main/java/com/example/kiosk/MilkTMenu.kt
@@ -4,11 +4,21 @@ class MilkTMenu {
     private val menuList = ArrayList<MenuItem>()
 
     init {
-        menuList.add(MenuItem("블랙",4500))
-        menuList.add(MenuItem("타로",4500))
-        menuList.add(MenuItem("얼그레이 티",4500))
-        menuList.add(MenuItem("제주그린티",4500))
-        menuList.add(MenuItem("초코",4500))
-        menuList.add(MenuItem("피스타치오",4900))
+        menuList.add(MenuItem("블랙 밀크티    ",4500))
+        menuList.add(MenuItem("타로 밀크티    ",4500))
+        menuList.add(MenuItem("얼그레이 밀크티 ",4500))
+        menuList.add(MenuItem("제주그린 밀크티 ",4500))
+        menuList.add(MenuItem("초콜릿밀크티    ",4500))
+        menuList.add(MenuItem("피스타치오 밀크티",4900))
+    }
+    fun displayMenu(){
+        println("[ MilkT MENU ]")
+    for((index, item)in menuList.withIndex()){
+        println("${index + 1}. ${item.name}   |${item.price} 원|")
+    }
+    println("0. 뒤로가기")
+    }
+    fun getMenuItem(index: Int): MenuItem?{
+        return if (index in 1..menuList.size) menuList[index - 1] else null
     }
 }
