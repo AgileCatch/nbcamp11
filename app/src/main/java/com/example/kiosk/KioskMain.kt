476295516b0eb658cac65cab3e267113d0c6404a
4seokhyeon <tktjrgus@naver.com>
2023-07-25 10:54:26 +0900

    장바구니 부분 커밋

--- app/src/main/java/com/example/kiosk/KioskMain.kt
+++ app/src/main/java/com/example/kiosk/KioskMain.kt
@@ -50,7 +50,7 @@ fun main() {
                 originalTMenu.displayMenu()
                 print("선택 ")
                 val originalTChoice = scanner.nextInt()
-                if (originalTChoice == 1) continue
+                if(originalTChoice == 1) continue
                 val menuItem = originalTMenu.getMenuItem(originalTChoice)
                 if (menuItem != null) {
                     order.addToOrder(menuItem)
@@ -94,6 +94,35 @@ fun main() {
                     println("잘못된 번호를 입력했어요. 다시 입력해주세요")
                 }
             }
+
+            6 -> {
+                if(order.isEmpty()){
+                    println("장바구니가 비어있습니다.")
+                    println("=====메뉴를 골라주세요")
+                    continue
+                }
+                order.displayOrder()
+                println("[ 장바구니 ]")
+                println("${order.getTotalPrice()}" + won)
+                println("1.  주문    2. 추가메뉴")
+                print("주문하려면 1번 다른 메뉴를 보고 싶으면 2번을 눌러주세요: ")
+                val orderChoice = scanner.nextInt()
+                when (orderChoice) {
+                    1 -> {
+                        if (order.getTotalPrice() <= cash) {
+                            cash -= order.getTotalPrice()
+                            println("주문이 완료되었습니다. 현재 잔액은 $cash 원 입니다.")
+                            order.clearOrder()
+                        } else {
+                            println("현재 잔액은 ${cash}원 으로 ${order.getTotalPrice() - cash}원이 부족하여 결제를 할 수 없습니다.")
+                        }
+                    }
+
+                    2 -> continue
+                    else -> println("잘못된 번호를 입력했어요. 다시 입력해주세요")
+                }
+            }
+
         }
     }
 
