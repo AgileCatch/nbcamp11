167279dd8fb5583362be130b04250d78ff34ab1f
yeonghyeon <agilecatch@gmail.com>
2023-07-27 21:53:33 +0900

    [FEAT] : ⚙️
    토핑추가 기능구현완료

--- app/src/main/java/com/example/kiosk/ToppingOption.kt
+++ app/src/main/java/com/example/kiosk/ToppingOption.kt
@@ -1,9 +1,14 @@
 package com.example.kiosk
 
+import java.util.Scanner
+
 // 전역 변수로 토핑 옵션들을 리스트에 저장
 val toppingOptions = listOf("펄", "화이트펄", "알로에", "코코넛", "밀크폼", "치즈폼")
+
 // 사용자가 선택한 토핑들을 저장하는 리스트
 val selectedToppings = mutableListOf<String>()
+var addToppings = mutableListOf<String>()
+
 
 class ToppingOption {
 
@@ -20,29 +25,59 @@ class ToppingOption {
         println("0. 뒤로가기 ")
     }
 
+
+    fun toppingmenu() {
+//        displayMenu()
+        addToppings.clear()
+        while (true) {
+            displayMenu()
+            print("토핑 선택 : ")
+            val toppingChoice = readLine()!!.toInt()
+            when (toppingChoice) {
+                0 -> {
+                    println("이전 메뉴로 돌아갑니다.")
+                    return // 함수를 종료하여 이전 메뉴로 돌아갑니다.
+                }
+
+                1, 2, 3, 4, 5, 6 -> {
+                    addTopping(toppingChoice)
+                    addToppings = selectedToppings
+                    println("선택된 토핑들: ${addToppings.joinToString(" ")}")
+                }
+
+                else -> {
+                    addTopping(toppingChoice)
+                    println("잘못된 선택입니다. 다시 선택해주세요.")
+                    // 잘못된 선택이므로 다시 함수를 호출하여 선택을 다시 받습니다.
+                }
+            }
+
+        }
+    }
+
     fun addTopping(toppingChoice: Int): Boolean {
         if (toppingChoice in 1..toppingOptions.size) {
             val selectedTopping = toppingOptions[toppingChoice - 1]
             if (selectedToppings.size < 3) {
                 selectedToppings.add(selectedTopping)
                 println("$selectedTopping 추가됨.")
+                return true
             } else {
                 println("\n\"최대 3개의 토핑까지 선택 가능합니다.\"")
+                return true
             }
-            return true
-        } else if (toppingChoice == 0) {
-            return true
         } else {
             println("잘못된 옵션 번호입니다.")
             return false
         }
-    }
 
-    fun getToppingOption(optionNumber: Int): String? {
-        if (optionNumber in 1..toppingOptions.size) {
-            return toppingOptions[optionNumber - 1]
-        }
-        return null
+
+//        fun getToppingOption(optionNumber: Int): String? {
+//            if (optionNumber in 1..toppingOptions.size) {
+//                return toppingOptions[optionNumber - 1]
+//            }
+//            return null
+//        }
+
     }
 }
-
